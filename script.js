6b224a570f3d05a4c88ffe036b0058fca951cfa7
Tachomdq <tachomdq@gmail.com>
2023-02-01 23:32:21 -0300

    Javascript, manejo del dom, eventos, iteraciones, manejo de collecciones, css js

---
+++ script.js
@@ -0,0 +1,35 @@
+const div = document.createElement("div");
+let theme = 1;
+
+div.appendChild(document.createTextNode("Cambiar Tema"));
+div.setAttribute("id", "boton");
+div.setAttribute("style", "width:65px ; height:45px ; background-color:#4B4B4D ; position:fixed ; right:15px ; bottom:15px ; color:white ; text-align: center ; border-radius:5px ; box-shadow:3px 3px 10px #4B4B4D ;");
+div.addEventListener("click", function () {
+    if (theme === 1) {
+        document.getElementById("photo").setAttribute("src", "./media/photo2.png");
+        const themeTopClass = document.getElementsByClassName("theme-top");
+        for (let i = 0; i < themeTopClass.length; i++) {
+            themeTopClass[i].setAttribute("style", "background-color:#8e8f0b ;");
+        }
+        const themeClass = document.getElementsByClassName("theme");
+        for (let i = 0; i < themeClass.length; i++) {
+            themeClass[i].setAttribute("style", "background-color:#b3b67e ;");
+        }
+        theme = 2;
+    }
+    else{
+        document.getElementById("photo").setAttribute("src", "./media/photo1.png");
+        const themeTopClass = document.getElementsByClassName("theme-top");
+        for (let i = 0; i < themeTopClass.length; i++) {
+            themeTopClass[i].setAttribute("style", "background-color:#f28938 ;");
+        }
+        const themeClass = document.getElementsByClassName("theme");
+        for (let i = 0; i < themeClass.length; i++) {
+            themeClass[i].setAttribute("style", "background-color:#f5ab73 ;");
+        }
+        theme = 1;
+    }
+});
+
+document.body.appendChild(div);
+
